e2902f6f87cfaa40e0d67702c103c10d8c2326bb
edDEVjr <edvaldodossantos737@gmail.com>
2025-03-17 13:42:30 -0300

    Add methods in the class NoticiaController

--- backend/src/controllers/direcao/NoticiaController.ts
+++ backend/src/controllers/direcao/NoticiaController.ts
@@ -40,7 +40,98 @@ class NoticiaController{
         }
     };
 
+    public listarNoticias = async (req: Request, res: Response): Promise<void> => {
+        try {
+          const resultado = await this.conexao?.query(
+            `SELECT n.*, f.nome as diretor_nome FROM museum.noticia n 
+              JOIN museum.diretor d ON n.diretor_id = d.id 
+              JOIN museum.funcionario f ON d.funcionario_id = f.id;`
+          );
     
+          if (resultado?.rowCount === 0) {
+            res.status(404).json({ erros: ["Nenhuma noticia encontrada!"] });
+            return;
+          }
+    
+          res.status(200).json(resultado?.rows);
+        } catch (erro: any) {
+          console.error(erro);
+          res.status(500).json({ erros: ["Não foi possível listar noticias!"] });
+        }
+    };
 
+    public buscarNoticiaPorId = async (
+        req: Request,
+        res: Response
+      ): Promise<void> => {
+        const { id } = req.params;
+    
+        try {
+          const resultado = await this.conexao?.query(
+            "SELECT n.*, f.nome as diretor_nome FROM museum.noticia i JOIN museum.diretor d ON i.diretor_id = d.id JOIN museum.funcionario f ON d.funcionario_id = f.id WHERE i.id = $1;",
+            [id]
+          );
+    
+          if (resultado?.rowCount === 0) {
+            res.status(404).json({ erros: ["Noticia não encontrada!"] });
+            return;
+          }
+    
+          res.status(200).json(resultado?.rows[0]);
+        } catch (erro: any) {
+          console.error(erro);
+          res.status(500).json({ erros: ["Não foi possível buscar noticia!"] });
+        }
+    };
 
+    public editarNoticia = async (req: Request, res: Response): Promise<void> => {
+        const { id } = req.params;
+        const { titulo, texto, palavrasChave } = req.body;
+    
+        try {
+          const resultado = await this.conexao?.query(
+            "UPDATE museum.noticia SET titulo = $1, texto = $2, palavrasChave = $3 WHERE id = $4 RETURNING *",
+            [titulo,texto,palavrasChave, id]
+          );
+    
+          if (resultado?.rowCount === 0) {
+            res.status(404).json({ erros: ["Noticia não encontrada!"] });
+            return;
+          }
+    
+          res.status(200).json({
+            mensagem: "Noticia atualizada com sucesso!",
+            item: resultado?.rows[0],
+          });
+        } catch (erro: any) {
+          console.error(erro);
+          res.status(500).json({ erros: ["Não foi possível editar noticia!"] });
+        }
+    };
+
+    public excluirNoticia = async (req: Request, res: Response): Promise<void> => {
+        const { id } = req.params;
+    
+        try {
+          const resultado = await this.conexao?.query(
+            "DELETE FROM museum.noticia WHERE id = $1",
+            [id]
+          );
+    
+          if (resultado?.rowCount === 0) {
+            res.status(404).json({ erros: ["Noticia não encontrada!"] });
+            return;
+          }
+    
+          res.status(200).json({
+            excluido: resultado?.rows[0],
+            mensagem: "Noticia excluída com sucesso!",
+          });
+        } catch (erro: any) {
+          console.error(erro);
+          res.status(500).json({ erros: ["Não foi possível excluir noticia!"] });
+        }
+    };
+    
 }
+export default NoticiaController;

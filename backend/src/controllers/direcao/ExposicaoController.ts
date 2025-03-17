e252d9fcec743d9321c7fae57fece9ceaaf4f24a
vitoriosantos00 <116050802+vitoriosantos00@users.noreply.github.com>
2025-03-17 00:00:40 -0300

    feat: alter ExposicaoController.ts

--- backend/src/controllers/direcao/ExposicaoController.ts
+++ backend/src/controllers/direcao/ExposicaoController.ts
@@ -26,15 +26,15 @@ class ExposicaoController {
         return;
       }
 
-      const gerente_id = req.cookies.usuario.gerente_id;
+      const diretor_id = req.cookies.usuario.diretor_id;
 
       this.exposicao.setTitulo(titulo);
       this.exposicao.setDescricao(descricao);
       this.exposicao.setDias(dias);
 
       const resultado = await this.conexao?.query(
-        "INSERT INTO museum.exposicao (titulo, dias, descricao VALUES ($1, $2, $3) RETURNING *",
-        [titulo, dias, descricao]
+        "INSERT INTO museum.exposicao (titulo, dias, descricao, diretor_id) VALUES ($1, $2, $3, $4) RETURNING *",
+        [titulo, dias, descricao, diretor_id]
       );
 
       res.status(201).json({
@@ -50,21 +50,20 @@ class ExposicaoController {
   public listarExposicao = async (req: Request, res: Response): Promise<void> => {
     try {
       const resultado = await this.conexao?.query(
-        `SELECT i.*, d.nome as diretor_exposicao
-         FROM museum.exposicao i 
-          JOIN museum.diretor d ON i.diretor_id = d.id 
-          GROUP BY museum.exposicao.dias;`
+        `SELECT e.*, f.nome as diretor_exposicao
+         FROM museum.exposicao e 
+          JOIN museum.diretor d ON e.diretor_id = d.id JOIN museum.funcionario f ON d.funcionario_id = f.id;`
       );
 
       if (resultado?.rowCount === 0) {
-        res.status(404).json({ erros: ["Nenhum Exposicao encontrado!"] });
+        res.status(404).json({ erros: ["Nenhum exposição encontrado!"] });
         return;
       }
 
       res.status(200).json(resultado?.rows);
     } catch (erro: any) {
       console.error(erro);
-      res.status(500).json({ erros: ["Não foi possível listar Exposicao!"] });
+      res.status(500).json({ erros: ["Não foi possível listar exposição!"] });
     }
   };
 
@@ -76,7 +75,7 @@ class ExposicaoController {
 
     try {
       const resultado = await this.conexao?.query(
-        "SELECT i.*, f.nome as funcionario_exposicao FROM museum.exposicao i JOIN museum.diretor g ON i.diretor_id = g.id JOIN museum.funcionario f ON g.funcionario_id = f.id WHERE i.id = $1;",
+        "SELECT e.*, f.nome as funcionario_exposicao FROM museum.exposicao e JOIN museum.diretor d ON e.diretor_id = d.id JOIN museum.funcionario f ON d.funcionario_id = f.id WHERE e.id = $1;",
         [id]
       );
 
@@ -98,12 +97,12 @@ class ExposicaoController {
 
     try {
       const resultado = await this.conexao?.query(
-        "UPDATE museum.exposicao SET titulo = $1, cod_Exposicao = $2, descricao = $3 WHERE id = $4 RETURNING *",
-        [titulo, dias, descricao, , id]
+        "UPDATE museum.exposicao SET titulo = $1, dias = $2, descricao = $3 WHERE id = $4 RETURNING *",
+        [titulo, dias, descricao, id]
       );
 
       if (resultado?.rowCount === 0) {
-        res.status(404).json({ erros: ["Exposiçâo não encontrado!"] });
+        res.status(404).json({ erros: ["Exposição não encontrado!"] });
         return;
       }
 
@@ -127,13 +126,13 @@ class ExposicaoController {
       );
 
       if (resultado?.rowCount === 0) {
-        res.status(404).json({ erros: ["Exposição não encontrado!"] });
+        res.status(404).json({ erros: ["Exposição não encontrada!"] });
         return;
       }
 
       res.status(200).json({
         excluido: resultado?.rows[0],
-        mensagem: "Exposição excluído com sucesso!",
+        mensagem: "Exposição excluída com sucesso!",
       });
     } catch (erro: any) {
       console.error(erro);

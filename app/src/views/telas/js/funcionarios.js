8dabc17914a09252ab0ebf120a7968019369e429
Luiz <luiz.saldanha@gmail.com>
2025-04-01 10:33:34 -0300

    feat: itens integrado

--- app/src/views/telas/js/funcionarios.js
+++ app/src/views/telas/js/funcionarios.js
@@ -1,3 +1,8 @@
+const usuario = JSON.parse(localStorage.getItem("usuario"));
+if (usuario.cargo !== "gerente") {
+  window.location.href = "/dashboard";
+}
+
 let paginaAtual = 1; // Página inicial
 const itensPorPagina = 10; // Número de funcionários por página
 
@@ -21,6 +26,14 @@ async function carregarFuncionarios() {
 let funcionarioSelecionado = null;
 function selecionarFuncionario(funcionario) {
   funcionarioSelecionado = funcionario;
+
+  const form = document.getElementById("form-editar");
+  form.nome.value = funcionarios.find(
+    (func) => func.funcionario_id === funcionario
+  ).nome;
+  form.email.value = funcionarios.find(
+    (func) => func.funcionario_id === funcionario
+  ).email;
 }
 
 // Renderiza a tabela com os dados
@@ -83,13 +96,13 @@ async function cadastrar() {
         throw await response.json();
       }
       alert("Funcionário cadastrado com sucesso!");
+
+      form.reset();
       window.location.reload();
     })
     .catch((error) => {
       alert(error.erros[0]);
     });
-
-  form.reset();
 }
 
 async function editar() {
@@ -113,13 +126,13 @@ async function editar() {
         throw await response.json();
       }
       alert("Funcionário editado com sucesso!");
+
+      form.reset();
       window.location.reload();
     })
     .catch((error) => {
       alert(error.erros[0]);
     });
-
-  form.reset();
 }
 
 async function excluir(id) {
@@ -145,8 +158,6 @@ async function excluir(id) {
     .catch((error) => {
       alert(error.erros[0]);
     });
-
-  form.reset();
 }
 
 const busca = document.getElementById("input-busca");
